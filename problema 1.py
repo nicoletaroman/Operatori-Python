0e2db0d0f4101f7f450b8d1c8c9c6f2c30bfb21e
nicoletaroman <70715983+nicoletaroman@users.noreply.github.com>
2020-09-17 13:18:24 +0300

    Add files via upload

---
+++ problema 1.py
@@ -0,0 +1,3 @@
+print ("Struguri     100     kg", "Mere         10      tone",\
+    "Cartofi      250     kg "\
+    , "Varza        1000     q" ,sep="\n")    
---
+++ problema 2.py
@@ -0,0 +1,8 @@
+a=5
+b=2
+print("suma=",a+b)
+print("diferenta=",a-b)
+print("catul impartirii=",a//b)
+print("restul impartirii=",a%b)
+print("produsul=",a*b)
+print("a la puterea b =",a**b)
