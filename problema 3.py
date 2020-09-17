5612761b118b379831de2728b245a0ad793f0010
nicoletaroman <70715983+nicoletaroman@users.noreply.github.com>
2020-09-17 14:10:23 +0300

    Delete problema 3.py

--- problema 3.py
+++
@@ -1,7 +0,0 @@
-lungimea_cubului=5
-perimetrul_cubului=12*lungimea_cubului
-aria_tuturor_suprafetelor=6*lungimea_cubului**2
-volumul_cubului=lungimea_cubului**3
-print( "perimetrul=",perimetrul_cubului)
-print( "aria totala=",aria_tuturor_suprafetelor)
-print( "volumul=",volumul_cubului)
