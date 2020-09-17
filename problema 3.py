c26579c1ccb84820dcc3cda971daa458ede4db52
nicoletaroman <70715983+nicoletaroman@users.noreply.github.com>
2020-09-17 14:13:17 +0300

    Add files via upload

---
+++ problema 3.py
@@ -0,0 +1,7 @@
+lungimea_cubului=5
+perimetrul_suprafatei=4*lungimea_cubului
+aria_tuturor_suprafetelor=6*lungimea_cubului**2
+volumul_cubului=lungimea_cubului**3
+print( "perimetrul unei suprafate=",perimetrul_suprafatei)
+print( "aria totala=",aria_tuturor_suprafetelor)
+print( "volumul=",volumul_cubului)
